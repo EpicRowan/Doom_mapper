43ae3dee5f0100d064239be3b2f12754876ac097
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-03-09 12:54:40 -0700

    Adjusted Bootstrap to make button active state

--- static/js/maps.js
+++ static/js/maps.js
@@ -141,16 +141,16 @@ dataLayerChoice.addEventListener("click", function (evt) {
 
 });
 
-var btns = btnContainer.getElementsByClassName("btn");
+// var btns = btnContainer.getElementsByClassName("btn");
 
-// Loop through the buttons and add the active class to the current/clicked button
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
 
 }
     
